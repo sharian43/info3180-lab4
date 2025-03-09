57f49c72363fbcf3b779c8f96c8dc6125a2a0fba
Sharian Johnson <shariant98@gmail.com>
2025-03-08 20:28:06 -0500

    completed exercise 6a

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
@@ -39,7 +39,7 @@ def upload():
             filename = secure_filename(image_file.filename)
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             image_file.save(image_path)
-            
+
             flash('File Saved', 'success')
             return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
@@ -71,6 +71,21 @@ def login():
             flash("Login successful")
             return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    uploads_dir = app.config['UPLOAD_FOLDER']
+    return send_from_directory(os.path.join(os.getcwd(),uploads_dir), filename)
+
+@app.route('/files')
+@login_required
+def files():
+    uploads_dir = app.config['UPLOAD_FOLDER']
+    images = get_uploaded_images()
+    print(images)
+    return render_template('files.html', images=images)
+
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
@@ -113,3 +128,14 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+##helper function
+
+def get_uploaded_images():
+    uploadDir = app.config['UPLOAD_FOLDER']
+    lst = []
+    for root, dirs, files in os.walk(uploadDir):
+        for file in files:
+            if file.endswith(('.jpg', '.jpeg', '.png')):
+                lst.append(os.path.join(root, file))
+    return lst
